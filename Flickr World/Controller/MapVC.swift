9e3b33400d70470522d5c0b3557c00d20a6174b2
alwongg <wong_1994@hotmail.com>
2017-11-25 14:10:51 -0600

    Initial app setup with MVC folders and Alamofire & Image cocoapods

---
+++ Flickr World/Controller/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Flickr World
+//
+//  Created by Alex Wong on 11/25/17.
+//  Copyright © 2017 Alex Wong. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,15 @@
+project 'Flickr World.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flickr World' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flickr World
+  
+  pod 'Alamofire'
+  pod 'AlamofireImage'
+
+end

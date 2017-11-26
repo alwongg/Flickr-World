6ad04c13239381b0cbe44d509fe74f4e5ddac9bd
alwongg <wong_1994@hotmail.com>
2017-11-25 20:12:42 -0600

    Add subviews programmatically and spinner animation

--- Flickr World/Controller/MapVC.swift
+++ Flickr World/Controller/MapVC.swift
@@ -15,54 +15,84 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     // MARK: - Outlets
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var pullUpView: UIView!
+    @IBOutlet weak var pullUpViewHeightConstraint: NSLayoutConstraint!
     
     // MARK: - Properties
     
-    var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
+    var locationManager = CLLocationManager()
+    var screenSize = UIScreen.main.bounds
+    
+    var spinner: UIActivityIndicatorView?
+    var progressLabel: UILabel?
     
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //set delegates
         mapView.delegate = self
         locationManager.delegate = self
         
+        //configure location services
         configureLocationServices()
+        
+        //show user location with blue dot
         mapView.showsUserLocation = true
         
+        //allow doubleTap function at load
         addDoubleTap()
-        
     }
     
     // MARK: - DoubleTap
     
     func addDoubleTap(){
+        
+        //set tap gesture recognizer
         let doubleTap = UITapGestureRecognizer(target: self, action: #selector(MapVC.dropPin(sender:)))
         doubleTap.numberOfTapsRequired = 2
         doubleTap.delegate = self
-        mapView.addGestureRecognizer(doubleTap)
         
+        //add tap gesture to mapView
+        mapView.addGestureRecognizer(doubleTap)
     }
     
     @objc func dropPin(sender: UITapGestureRecognizer){
-        print("pin was dropped")
-        removePin()
         
-        //touchPoint = point on phone screen
+        //update UI
+        removePin()
+        animateViewUp()
+        addSwipe()
+        addSpinner()
+        
+        //touchPoint = point on phone screen in (x,y)
         let touchPoint = sender.location(in: mapView)
         print(touchPoint)
         
         //convert touchPoint to mapCoordinate
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
         
+        //create annotation
         let annotation = DroppablePin(coordinate: touchCoordinate, identifier: "droppablePin")
+        
+        //add annotation to mapView
         mapView.addAnnotation(annotation)
         
+        //create region
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
+        
+        //add region to mapView
         mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    func animateViewUp(){
+        pullUpViewHeightConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     func removePin(){
@@ -71,30 +101,56 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
+    // MARK: - Swipe Down Dismiss
+    
+    func addSwipe(){
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(MapVC.animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+    }
+    
+    @objc func animateViewDown(){
+        pullUpViewHeightConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            self.view.layoutIfNeeded()
+        }
+    }
+    
+    // MARK: - Spinner
+    
+    func addSpinner(){
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        spinner?.startAnimating()
+        pullUpView.addSubview(spinner!)
+    }
+    
     // MARK: - Actions
-
-    @IBAction func zoomToUserLocation(_ sender: Any) {
+    
+    @IBAction func centerMapLocation(_ sender: Any) {
         if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse{
             centerMapOnUserLocation()
         }
     }
-    
 }
+
+// MARK: - MapView Delegate
 
 extension MapVC: MKMapViewDelegate{
     
-    // customize annotations
-    
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
         if annotation is MKUserLocation {
             return nil
         }
+        
         let pinAnnotation = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "droppablePin")
         pinAnnotation.pinTintColor = UIColor.orange
         pinAnnotation.animatesDrop = true
         return pinAnnotation
     }
-    
     
     func centerMapOnUserLocation(){
         guard let coordinate = locationManager.location?.coordinate else {return}
@@ -103,10 +159,12 @@ extension MapVC: MKMapViewDelegate{
         
         mapView.setRegion(coordinateRegion, animated: true)
     }
-    
 }
 
+// MARK: - Location Delegate
+
 extension MapVC: CLLocationManagerDelegate{
+    
     func configureLocationServices(){
         if authorizationStatus == .notDetermined{
             locationManager.requestAlwaysAuthorization()
@@ -114,6 +172,7 @@ extension MapVC: CLLocationManagerDelegate{
             return
         }
     }
+    
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         centerMapOnUserLocation()
     }
